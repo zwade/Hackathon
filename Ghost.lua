c1c7fbee07324accdb4aa75c8ff92651cfb65368
2015rjain <rishubjain@gmail.com>
2013-04-27 05:46:00 -0300

    Update Ghost.lua

--- Ghost.lua
+++ Ghost.lua
@@ -5,27 +5,27 @@ function Ghost:initialize(nx,ny,template,map,prot)
 end
 
 function Ghost:behave(keys,dt)
-	if math.abs(self.x-self.protagonist.x)<200 then
-                if self.x > self.protagonist.x then
+	if math.abs(self.x+40-self.protagonist.x)<200 then
+                if self.x+40 > self.protagonist.x then
                         self.vx = self.vx - 40
                 else
                         self.vx = self.vx + 40
                 end
         else
-                if self.x > self.protagonist.x then
+                if self.x+40 > self.protagonist.x then
                         self.vx = self.vx - 25
                 else
                         self.vx = self.vx + 25
                 end
 	end
-	if math.abs(self.y-self.protagonist.y)<200 then
-		if self.y > self.protagonist.y then
+	if math.abs(self.y+35-self.protagonist.y)<200 then
+		if self.y+35 > self.protagonist.y then
 			self.vy = self.vy - 5
 		else 
 			self.vy = self.vy + 5
 		end
 	else
-		if self.y > self.protagonist.y then
+		if self.y+35 > self.protagonist.y then
                         self.vy = self.vy - 2
                 else
                         self.vy = self.vy + 2
