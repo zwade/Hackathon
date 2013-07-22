2dbd92f086d69ec8eafddf3efaa48d063819639d
lexical_enumeration <bzhang.2003@gmail.com>
2013-07-22 16:24:32 -0400

    Was annoyed by eclipse errors so here ya go

--- SawC.lua
+++ SawC.lua
@@ -1,13 +1,14 @@
 require("component")
 require("damagemix")
+
 local num=false
-SawC = Component:subclass("SawrC")
+SawC = Component:subclass("SawC")
 SawC:include(meleeDamage)
 function SpiderC:initialize()
   Component.initialize(self,"Spike1.png",0,0,true)
-
 end
-function SawC:render(sx,sy)
+
+--[[function SawC:render(sx,sy)
   if self.parent.x<self.parent.protagonist.x then
 		sx = -1
 	else 
@@ -18,7 +19,10 @@ function SawC:render(sx,sy)
 		love.graphics.draw(self.image,self.parent.x+tmpx,self.parent.y+tmpy, self.dir, sx,sy,self.ox,self.oy)
 	else
 		love.graphics.draw("Spike2.png",self.parent.x+tmpx,self.parent.y+tmpy, self.dir, sx,sy,self.ox,self.oy)
-end
+end]]--
+
+--didn't know what was causing the error
+
 function SawC:init(parent)
 	--parent.speed = 100
 end
